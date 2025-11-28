c8c46fa6452ec666cd5b5e49e32890bca16b3603
anhvuduc <anh.vdc@outlook.com>
2025-11-28 22:44:30 +0700

    fix download issue + VI export issue 3

--- download/config/config.py
+++ download/config/config.py
@@ -27,9 +27,9 @@ LOCAL_DATA_DIR = os.getenv('DATA_DIR', '/opt/data')
 # Mapping: Tên thành phố (viết thường) -> Asset ID trên GEE
 ROIS = {
     'hanoi': 'users/anhvuduc/hanoi',
-    # 'haiphong': 'users/anhvuduc/haiphong_blv',
+    'haiphong': 'users/anhvuduc/haiphong_blv',
     # 'danang': 'users/anhvuduc/dn_hs',
-    'hcm': 'users/anhvuduc/hcm'
+    # 'hcm': 'users/anhvuduc/hcm'
     # 'binhduong': 'users/anhvuduc/binhduong' # (Optional: Uncomment nếu cần)
 }
 
